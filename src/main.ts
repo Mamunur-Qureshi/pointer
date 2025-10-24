eb9ae7d115e45085599225c794a978e9b276b267
Mamunur Qureshi <mamunur.qureshi@croneri.co.uk>
2025-10-24 08:34:46 +0100

    "main: Final touches"

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import './style.css'
 
-import { handleReset, setupCounter } from './counter.ts'
+import { handleReset, setupCounter } from './pointer.ts'
 
-export const SIZE = 100;
+export const SIZE = 75;
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="main">
-    <div id="actor" style=></div>
+    <div class="wrapper">
+      <div id="actor" style=></div>
+    </div>
     <button id="reset">Reset</button>
   </div>
 `
---
+++ src/pointer.ts
@@ -0,0 +1,71 @@
+import PointerTracker from "pointer-tracker";
+import { SIZE } from "./main";
+
+const MOVE_LIMIT = 20;
+const BUTTON_SIZE = 50;
+
+export function setupCounter(element: HTMLButtonElement) {
+  const pointerTracker = new PointerTracker(element, {
+    start(pointer, event) {
+      element.classList.add("selected");
+      const rect = element.getBoundingClientRect();
+      element.style.left = `${rect.x}px`;
+      element.style.top = `${rect.y}px`;
+      element.style.position = `absolute`;
+      return true;
+    },
+    move(previousPointers, changedPointers, event) {
+      const screenWidth = window.innerWidth;
+      const screenHeight = window.innerHeight;
+      if (previousPointers.length > 1) {
+        const width = element.offsetWidth;
+        const {
+          clientX: x1,
+          clientY: y1
+        } = previousPointers[0];
+        const {
+          clientX: x2,
+          clientY: y2
+        } = previousPointers[1];
+        const {
+          clientX: xc,
+          clientY: yc
+        } = changedPointers[0];
+        const boundary = element.getBoundingClientRect();
+        const distance1 = (x1 - x2) ** 2 + (y1 - y2) ** 2
+        const distance2 = (xc - x2) ** 2 + (yc - y2) ** 2
+        if (width > 20 && distance1 > distance2) {
+          element.style.width = `${element.offsetWidth - 2}px`;
+          element.style.height = `${element.offsetHeight - 2}px`;
+        } else {
+          if (width < SIZE * 4 && boundary.right < screenWidth - MOVE_LIMIT && boundary.bottom < screenHeight - BUTTON_SIZE) {
+            element.style.width = `${element.offsetWidth + 2}px`;
+            element.style.height = `${element.offsetHeight + 2}px`;
+          }
+        }
+      } else {
+        const {
+          clientX: x,
+          clientY: y
+        } = changedPointers[0];
+        if (x > MOVE_LIMIT && x < screenWidth - (SIZE + MOVE_LIMIT)) {
+          element.style.left = `${x}px`;
+        }
+        if (y > MOVE_LIMIT && y < screenHeight - (SIZE + BUTTON_SIZE)) {
+          element.style.top = `${y}px`;
+        }
+      }
+    },
+    end() {
+      element.className = "";
+    },
+    avoidPointerEvents: true
+  })
+}
+
+export function handleReset(element: HTMLButtonElement) {
+  const actor = document.querySelector<HTMLButtonElement>('#actor')!
+  element.onclick = (event) => {
+    actor.removeAttribute("style")
+  }
+}
